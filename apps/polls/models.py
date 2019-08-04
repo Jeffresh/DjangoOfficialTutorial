37380bc6b556561e21e8d71fd13669bb9215caf3
Jeff <jeff.uca@gmail.com>
2019-08-04 13:39:28 +0200

    added new method was published recently to Question model of polls app

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 
 # Create your models here.
@@ -7,9 +9,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def __str__(self):
         return self.question_text
-
 
 
 class Choice(models.Model):
