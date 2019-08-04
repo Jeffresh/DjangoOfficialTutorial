8d63825eb98bdac5653d5a0cd091b0c1b82ebef4
Jeff <jeff.uca@gmail.com>
2019-08-04 16:55:12 +0200

    Agregada plantilla detail e index de app polls, y agregadas las conexiones a urls y views para su funcionamiento

--- apps/polls/views.py
+++ apps/polls/views.py
@@ -3,14 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
-def details(request, question_id):
-    return HttpResponse("You're looking at questions %s." % question_id)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
